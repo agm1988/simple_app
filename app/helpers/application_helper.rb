13d37b3727367004cdd67ae4f6c5b6dcd2cbabbc
Alexandr <Gsashko@mail.ru>
2011-12-20 19:16:14 +0200

    design fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def title
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = "Twitter"
     if @title.nil?
       base_title
     else
@@ -10,7 +10,7 @@ module ApplicationHelper
   end
 
   def logo
-      image_tag("logo.png", :alt => "Sample App", :class => "round")
+      image_tag("logo.png", :alt => "Twitter", :class => "round")
   end
 
 end
